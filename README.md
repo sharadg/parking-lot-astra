7ca1b1cd170d9dc86aefb2f02acd28eaaf7f6413
sharadg <91043+sharadg@users.noreply.github.com>
2021-01-03 15:34:39 -0600

    mvp commit~

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
+<<<<<<< HEAD
 # parking-lot-astra
+=======
+## SensorsRUs MVP - Parking Lot Occupancy
+
+This simple codebase allows one to connect to Datastax Astra and pull down specific query results for the number of open slots at a particular floor of a specific Parking Lot.
+
+This github repo is setup to deploy continuously to [Google Cloud Run](https://parking-lot-astra-ynv6tadumq-ue.a.run.app/) and can be called like below:
+
+```shell
+http -v https://parking-lot-astra-ynv6tadumq-ue.a.run.app/slots/nasa_east/1
+
+GET /slots/nasa_east/1 HTTP/1.1
+Accept: */*
+Accept-Encoding: gzip, deflate
+Connection: keep-alive
+Host: parking-lot-astra-ynv6tadumq-ue.a.run.app
+User-Agent: HTTPie/2.3.0
+
+
+
+HTTP/1.1 200 OK
+Alt-Svc: h3-29=":443"; ma=2592000,h3-T051=":443"; ma=2592000,h3-Q050=":443"; ma=2592000,h3-Q046=":443"; ma=2592000,h3-Q043=":443"; ma=2592000,quic=":443"; ma=2592000; v="46,43"
+Content-Length: 68
+Date: Sun, 03 Jan 2021 21:20:23 GMT
+Server: Google Frontend
+X-Cloud-Trace-Context: 65cbc3a241aad091b5b59b6837cda159
+content-type: text/html; charset=utf-8
+
+Total slots available for FloorNum: 1 at Parking Lot nasa_east are 4
+```
+>>>>>>> 615d73a (mvp commit)
---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+
+from flask import Flask
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from flask import jsonify
+
+app = Flask(__name__)
+
+cloud_config = {
+    'secure_connect_bundle': './creds.zip'
+}
+auth_provider = PlainTextAuthProvider('petclinic', 'petclinic')
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+session.set_keyspace("petclinickp")
+
+
+@app.route("/slots/<parking_lot>/<floor_num>", methods=["GET"])
+def lookup_available_slots(parking_lot, floor_num):
+    global session
+    query = 'SELECT parking_lot, floor_num, sensor_slot, max(recorded_time), occupied FROM parking_lot_occupancy WHERE parking_lot=\'{parking_lot}\' AND floor_num = {floor_num} GROUP BY parking_lot, floor_num, sensor_slot ALLOW FILTERING ;'.format(
+        parking_lot=parking_lot, floor_num=floor_num)
+    rows = session.execute(query).all()
+
+    if rows:
+        slots = 0
+        for row in rows:
+            if not bool(row.occupied):
+                slots += 1
+            # print('Parking Lot: {pl}, FloorNum: {fn}, SensorSlot: {ss}, Occupied: {oc}'.format(pl=row.parking_lot, fn=row.floor_num, ss=row.sensor_slot, oc=row.occupied))
+        return 'Total slots available for FloorNum: {} at Parking Lot {} are {}'.format(floor_num, parking_lot, slots), 200
+    else:
+        return "An error occurred", 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
