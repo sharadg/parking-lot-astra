69aef0450d5e56c5b7de6ef2e4f14c30af7f2911
sharadg <91043+sharadg@users.noreply.github.com>
2021-04-13 16:49:35 -0500

    removed the cassandra-driver dependency and using Astra REST APIs only, also added pip install from requirements.txt to Dockerfile

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 import os
-
 from flask import Flask
-from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
 from flask import jsonify
+import requests
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Astra Cluster ID
+astraDbId = os.environ.get("ASTRA_DB_ID", "xxx")
+# Astra DB region
+astraRegion = os.environ.get("ASTRA_DB_REGION", "xxx")
+# Astra DB keyspace
+astraKeyspace = os.environ.get("ASTRA_DB_KEYSPACE", "xxx")
+# Astra collection (think of it like a table) to create
+astraTable = os.environ.get("ASTRA_DB_TABLE", "xxx")
+# App token
+astraAppToken = os.environ.get("ASTRA_DB_APPLICATION_TOKEN", "xxx")
+
 
 app = Flask(__name__)
-
-cloud_config = {
-    'secure_connect_bundle': '/tmp/secure-connect-petclinic.zip'
-}
-auth_provider = PlainTextAuthProvider('gYkAszLdRdNZfPqFaLTUmUem', 'tCNepGtye_Whk9+jcGeFCo,ZyIiRwk5_1E8AZHwidFpk+KWrQnoGkS752UJ15nvTy5Z,X9jZ7,Ayg5ayEjj1Q2p.qWkLFfbCTcr.BalxmfpqlYDCo5Yxcv4l+2ZcWakZ')
-cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
-session = cluster.connect()
-session.set_keyspace("petclinickp")
 
 
 @app.route("/slots/<parking_lot>/<floor_num>", methods=["GET"])
 def lookup_available_slots(parking_lot, floor_num):
-    global session
-    query = 'SELECT parking_lot, floor_num, sensor_slot, max(recorded_time), occupied FROM parking_lot_occupancy WHERE parking_lot=\'{parking_lot}\' AND floor_num = {floor_num} GROUP BY parking_lot, floor_num, sensor_slot ALLOW FILTERING ;'.format(
-        parking_lot=parking_lot, floor_num=floor_num)
-    rows = session.execute(query).all()
+    http_request = "https://" + astraDbId + "-" + astraRegion + ".apps.astra.datastax.com/api/rest/v2/keyspaces/" + \
+        astraKeyspace + "/" + astraTable + "/" + parking_lot + "/" + \
+        floor_num + "?page-size=1&raw=true&fields=num_available"
+    headers = {"x-cassandra-token": astraAppToken}
+    response_get_availability = requests.get(http_request, headers=headers)
 
-    if rows:
-        slots = 0
-        for row in rows:
-            if not bool(row.occupied):
-                slots += 1
-            # print('Parking Lot: {pl}, FloorNum: {fn}, SensorSlot: {ss}, Occupied: {oc}'.format(pl=row.parking_lot, fn=row.floor_num, ss=row.sensor_slot, oc=row.occupied))
-        return 'Total slots available for FloorNum: {} at Parking Lot {} are {}'.format(floor_num, parking_lot, slots), 200
+    if response_get_availability.status_code == 200:
+        response_availability = json.loads(response_get_availability.text)
+        return 'Total slots available for FloorNum: {} at Parking Lot {} are {}'.format(floor_num, parking_lot, response_availability[0]['num_available']), response_get_availability.status_code
     else:
-        return "An error occurred", 500
+        return "An error occurred", response_get_availability.status_code
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
+    app.run(debug=True, host="0.0.0.0", port=int(
+        os.environ.get("PORT", "8080")))
